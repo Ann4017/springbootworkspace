814d48948a13ca09fbbc47587c6cae0f83a1d54f
ann4017 <abh4017@naver.com>
2024-01-12 18:23:08 +0900

    commit

--- board/src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ board/src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -3,11 +3,17 @@ package org.zerock.board.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
+
+import java.util.List;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     @Modifying
     @Query("delete from Reply r where r.board.bno = :bno")
     void deleteByBno(Long bno);
+
+    // 쿼리메소드 형식 특정 게시글의 전체 댓글 조회
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
